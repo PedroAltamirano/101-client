4f614805d29659c0d6e86691933b26ad95084a5a
PedroAltamirano <pedroaal04@gmail.com>
2021-12-06 11:52:44 -0500

    added ui

--- src/movies.js
+++ src/movies.js
@@ -2,6 +2,8 @@ import * as React from 'react';
 
 import './style/movies.css'
 
+import { Typography, IconButton } from '@mui/material';
+
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -10,38 +12,69 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+import Stack from '@mui/material/Stack';
+
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+
+function createData(id, name, description, duration, genre) {
+  return { id, name, description, duration, genre };
 }
 
+const rows = [
+  createData(1, 'Frozen yoghurt', 'Descripcion 1...', 120, 'genero 1'),
+  createData(2, 'Ice cream sandwich', 'Descripcion 2...', 120, 'genero 2'),
+  createData(3, 'Eclair', 'Descripcion 3..', 120, 'genero 3'),
+  createData(4, 'Cupcake', 'Descripcion 4...', 120, 'genero 1'),
+  createData(5, 'Gingerbread', 'Descripcion 5...', 120, 'genero 2'),
+];
+
 const Movies = () => {
+
+  const openDetail = data => { console.log('detail') }
+  const openNew = () => { console.log('new') }
+  const openEdit = data => { console.log('edit') }
+
   return (
     <>
-      <h1>Películas</h1>
+      <Stack spacing={2} direction="row" justifyContent="space-between">
+        <Typography variant="h2" component='h2' mt={2}>Películas</Typography>
+        <IconButton aria-label="nueva película" size="large" onClick={openNew}>
+          <AddIcon fontSize="inherit" />
+        </IconButton>
+      </Stack>
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell>Dessert (100g serving)</TableCell>
-              <TableCell align="right">Calories</TableCell>
-              <TableCell align="right">Fat&nbsp;(g)</TableCell>
-              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+              <TableCell>Nombre</TableCell>
+              <TableCell>Duración (minutos)</TableCell>
+              <TableCell>Género</TableCell>
+              <TableCell>Acciones</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {rows.map((row) => (
               <TableRow
-                key={row.name}
+                key={row.id}
                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
               >
                 <TableCell component="th" scope="row">
                   {row.name}
                 </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
+                <TableCell>{row.duration}</TableCell>
+                <TableCell>{row.genre}</TableCell>
+                <TableCell>
+                  <Stack spacing={2} direction="row">
+                    <IconButton aria-label="ver película" onClick={row => openDetail(row)}>
+                      <VisibilityIcon />
+                    </IconButton>
+                    <IconButton aria-label="editar película" onClick={row => openEdit(row)}>
+                      <EditIcon />
+                    </IconButton>
+                  </Stack>
+                </TableCell>
               </TableRow>
             ))}
           </TableBody>
