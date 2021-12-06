a20e02300f919f0b750bb9f5112f4b4dd1b2f428
PedroAltamirano <pedroaal04@gmail.com>
2021-12-06 14:17:35 -0500

    added actor page + modal

--- src/actors.js
+++ src/actors.js
@@ -1,22 +1,20 @@
 import * as React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import './style/actors.css'
+import BaseTable from './components/baseTable';
+import ActorForm from './components/actorForm';
 
-import { Typography, IconButton, Button } from '@mui/material';
+import { IconButton, Button } from '@mui/material';
 
-import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 
 import Stack from '@mui/material/Stack';
 
-import VisibilityIcon from '@mui/icons-material/Visibility';
-import AddIcon from '@mui/icons-material/Add';
 import EditIcon from '@mui/icons-material/Edit';
 
 function createData(id, img, name, age) {
@@ -32,55 +30,56 @@ const rows = [
 ];
 
 const Actors = () => {
+  const [form, setForm] = useState(false)
+  const [data, setData] = useState()
 
-  const openDetail = data => { console.log('detail') }
-  const openNew = () => { console.log('new') }
-  const openEdit = data => { console.log('edit') }
+  const openForm = data => {
+    setData(data)
+    setForm(true)
+  }
+
+  const closeForm = () => {
+    setForm(false)
+    setData(null)
+  }
 
   return (
     <>
-      <Stack spacing={2} direction="row" justifyContent="space-between">
-        <Typography variant="h2" component='h2' mt={2}>Actores</Typography>
-        <IconButton aria-label="nuevo actor" size="large" onClick={openNew}>
-          <AddIcon fontSize="inherit" />
-        </IconButton>
-      </Stack>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Foto</TableCell>
-              <TableCell>Nombre</TableCell>
-              <TableCell>Edad</TableCell>
-              <TableCell>Acciones</TableCell>
+      <BaseTable title='Actores' addAction={openForm}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Foto</TableCell>
+            <TableCell>Nombre</TableCell>
+            <TableCell>Edad</TableCell>
+            <TableCell>Acciones</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.id}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.img}
+              </TableCell>
+              <TableCell>{row.name}</TableCell>
+              <TableCell>{row.age}</TableCell>
+              <TableCell>
+                <Stack spacing={2} direction="row">
+                  <IconButton aria-label="editar actor" onClick={() => openForm(row)}>
+                    <EditIcon />
+                  </IconButton>
+                </Stack>
+              </TableCell>
             </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-              <TableRow
-                key={row.id}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {row.img}
-                </TableCell>
-                <TableCell>{row.name}</TableCell>
-                <TableCell>{row.age}</TableCell>
-                <TableCell>
-                  <Stack spacing={2} direction="row">
-                    <IconButton aria-label="editar actor" onClick={row => openEdit(row)}>
-                      <EditIcon />
-                    </IconButton>
-                  </Stack>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+          ))}
+        </TableBody>
+      </BaseTable>
       <Stack direction='row-reverse' sx={{ mt: 2 }}>
         <Link to='/'><Button variant="outlined">Películas</Button></Link>
       </Stack>
+      <ActorForm data={data} open={form} handleClose={closeForm} />
     </>
   )
 }
---
+++ src/components/actorForm.js
@@ -0,0 +1,83 @@
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import { Stack } from '@mui/material';
+import TextField from '@mui/material/TextField';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+const ActorForm = ({ data, open, handleClose }) => {
+  const initialState = { name: '', age: '', img: '' }
+  const [form, setForm] = useState(initialState)
+
+  useEffect(() => {
+    if (data) setForm(data);
+    else setForm(initialState)
+  }, [data])
+
+  const handleInput = event => {
+    console.log(event.target)
+    setForm({ ...form, [event.target.name]: event.target.value })
+    return
+  }
+
+  return (
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <Typography id="modal-modal-title" variant="h4" fontWeight='bold'>
+          Nueva Película
+        </Typography>
+        <Box
+          component="form"
+          noValidate
+          autoComplete="off"
+          sx={{ width: 1, mb: 2 }}
+        >
+          <TextField
+            fullWidth
+            multiline
+            maxRows={4}
+            id="nombre"
+            name="name"
+            label="Nombre"
+            sx={{ my: 1 }}
+            value={form.name}
+            onChange={handleInput} />
+          <TextField
+            fullWidth
+            id="edad"
+            name="age"
+            label="Edad"
+            sx={{ my: 1 }}
+            value={form.age}
+            onChange={handleInput} />
+        </Box>
+        <Stack direction='row-reverse' gap={2}>
+          <Button variant='contained' color='primary'>Guardar</Button>
+          <Button variant='contained' color='error'>Cancelar</Button>
+        </Stack>
+      </Box>
+    </Modal>
+  )
+}
+
+export default ActorForm
