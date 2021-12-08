1a9b1d7ddb110818207c98b11a5f0860de405e0b
PedroAltamirano <pedroaal04@gmail.com>
2021-12-08 00:08:29 -0500

    added .env

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,8 @@
-const baseURL = 'http://localhost:3000/'
+require('dotenv').config()
+
+let baseURL = 'http://localhost:3000/'
+if (process.env.APP_ENV === 'production')
+  baseURL = 'https://101-server.vercel.app/'
 const baseAPI = `${baseURL}api/`
 
 module.exports = {
